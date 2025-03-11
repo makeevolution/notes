c8df717851081818f3261639110436f9b0e5fb85
aldo <aldo_hasibuan@yahoo.com>
2025-03-11 19:20:29 +0100

    created our own Task, with detailed notes on what is goign on

--- csharp/async await/async-await-in-detail/4. Creating Task from Scratch/src/CreatingTaskFromScratch/AldoTask.cs
+++ csharp/async await/async-await-in-detail/4. Creating Task from Scratch/src/CreatingTaskFromScratch/AldoTask.cs
@@ -13,7 +13,7 @@ public class AldoTask
 
 	bool _completed;
 	Exception? _exception;
-	Action? _action;
+	Action? _continueWithAction;
 	ExecutionContext? _context;
 
 	public bool IsCompleted
@@ -51,15 +51,24 @@ public class AldoTask
 		return task;
 	}
 
+	/* This method is used to delay execution of the task, without blocking the calling thread */
+	public static AldoTask Delay(TimeSpan delay)
+	{
+		var task = new AldoTask();
+		// We use a Timer so that the calling thread is not blocked; TPL will use next available thread after the delay to execute the task.
+		new Timer(_ => task.SetResult()).Change(delay, Timeout.InfiniteTimeSpan);
+		return task;
+	}
+
 	/* This method is a continuation method that runs a follow-up action i.e. a callback after the task is completed 
 	The first call to this method (i.e. in Program.cs) will most likely become the registration of the callback (i.e. _completed false case below)
 	, but the callback can indeed be immediately executed if the main task is already completed (i.e. _completed true case below) 
-	In the case of registration, once the main task is complete, the SetResult method will be called, with _action defined.
+	In the case of registration, once the main task is complete, the SetResult method will be called, with _continueWithAction defined.
 	See CompleteTask method; if _action is defined, then you see there the callback is Invoked.
 
 	*/
 	public AldoTask ContinueWith(Action action){
-		AldoTask task = new();
+		var task = new AldoTask();
 
 		lock (_lock)
 		{
@@ -69,7 +78,7 @@ public class AldoTask
 				{
 					try
 					{
-						Console.WriteLine("The main task is very fast and so is already completed!, executing callback");
+						Console.WriteLine($"The main task is very fast and so is already completed!, executing callback {action}");
 						action();
 						task.SetResult();
 					}
@@ -81,13 +90,50 @@ public class AldoTask
 			}
 			else
 			{
-				Console.WriteLine("The main task is slow and not completed yet, so we will store the action in _action variable and the execution context in _context variable");
-				_action = action;
+				Console.WriteLine("The main task is slow and not completed yet, so we will store the action in _continueWithAction variable and the execution context in _context variable");
+				_continueWithAction = action;
 				_context = ExecutionContext.Capture();
 			}
 		}
 		return task;
 	}
+
+	/* This method is used to BLOCK the current thread until the task is completed.
+	*/
+	public void Wait() {
+		// Wait until the task is completed.
+		// A ManualResetEventSlim allows threads to wait for a signal from another thread before continuing execution. 
+		// In other words, the ManualResetEventSlim class allows you to synchronize the execution of multiple threads by using set signals.
+		// How does this work concretely:
+
+		// Before we use this Wait method, logically we would've called either Run or Delay method to execute some stuff, and it will stop at the action() call (see the body of these methods)
+		// Now, see the body below; while the task we called is not yet completed, here we create a ResetEventSlim and call it's Wait method.
+		// But before calling it, we leverage our ContinueWith and create a callback to set the ResetEventSlim, which is then registered in _continueWithAction.
+		
+		// Now, when action(); in Run or Delay is complete, the next line there calls the task.SetResult() method, which calls CompleteTask private method.
+		// See the CompleteTask method; there we check if _continueWithAction is not null, and if so, we run it.
+		// This is indeed the case (we leveraged ContinueWith as explained above!)
+		// Thus, ExecutionContext.Run will run the set ResetEventSlim callback, which will unblock the resetEventSlim's Wait method, and thus the calling thread will continue executing.
+		ManualResetEventSlim? resetEventSlim = null;
+		lock (_lock)
+		{
+			if (!_completed)
+			{
+				resetEventSlim = new();
+				ContinueWith(() => resetEventSlim.Set());
+			}
+		}
+		resetEventSlim?.Wait();
+
+		if (_exception is not null)
+		{
+			// Use ExceptionDispatchInfo to preserve the original stack trace.
+			ExceptionDispatchInfo.Throw(_exception);
+		}
+	}
+
+
+	#region Helper Sections
 
 	public void SetResult() => CompleteTask(null);
 
@@ -103,17 +149,42 @@ public class AldoTask
 			}
 			_exception = exception;
 			_completed = true;
-			if (_action is not null)
+			if (_continueWithAction is not null)
 			{
 				if (_context is null)
 				{
-					_action.Invoke();
+					_continueWithAction.Invoke();
 				}
 				else
 				{
-					ExecutionContext.Run(_context, state => ((Action?)state)?.Invoke(), _action);
+					Console.WriteLine($"running action {_continueWithAction.Method}");
+					ExecutionContext.Run(_context, state => ((Action?)state)?.Invoke(), _continueWithAction);
 				}
 			}
 		}
 	}
+
+	#endregion
+
+	#region Awaiter Section, the magic behind the await keyword
+
+	// We can use the await method for our custom task, if our custom task implements a method called GetAwaiter that
+	// returns an struct that implements INotifyCompletion.
+
+	public AldoTaskAwaiter GetAwaiter() => new(this);
+
+	#endregion
+}
+
+public readonly struct AldoTaskAwaiter : INotifyCompletion
+{
+	readonly AldoTask _task;
+
+	public AldoTaskAwaiter(AldoTask task) => _task = task;
+
+	public bool IsCompleted => _task.IsCompleted;
+
+	public void OnCompleted(Action continuation) => _task.ContinueWith(continuation);
+
+	public void GetResult() => _task.Wait();
 }
--- csharp/async await/async-await-in-detail/4. Creating Task from Scratch/src/CreatingTaskFromScratch/Program.cs
+++ csharp/async await/async-await-in-detail/4. Creating Task from Scratch/src/CreatingTaskFromScratch/Program.cs
@@ -2,32 +2,80 @@
 
 using CreatingTaskFromScratch;
 var useAldo = true;
-if (useAldo) {
+if (useAldo)
+{
     Console.WriteLine("Start simulation of our own implementation of Task class");
     Console.WriteLine("Read the code along and understand how it works");
     Console.WriteLine("Type enter to continue on every step");
     Console.ReadLine();
 
-    Console.WriteLine("For this first one, notice that the thread IDs are different, since the AldoTask.Run method uses ThreadPool.QueueUserWorkItem, thus using a background thread");
+    Console.WriteLine("For this first one, we are investigating Run method.");
+    Console.WriteLine("Notice that the thread IDs are different, since the AldoTask.Run method uses ThreadPool.QueueUserWorkItem, thus using a background thread");
     Console.WriteLine($"Starting Thread Id: {Thread.CurrentThread.ManagedThreadId}");
     AldoTask.Run(() => Console.WriteLine($"First AldoTask Id: {Thread.CurrentThread.ManagedThreadId}"));
-    
+    Console.WriteLine("Main thread: I can be printed before the first AldoTask finishes (not necessarily always the case; depends on speed of CPU etc.). See Wait() section below to see how to make the main thread always wait for the background to finish");
+
     Console.ReadLine();
 
-    Console.WriteLine("For this second one, notice that the (background) thread IDs that execute the two are the same, since the first action quickly finishes, and thus is available "
+    Console.WriteLine("For this second one, we are implementing ContinueWith. Study also the docstring of ContinueWith in AldoTask to truly understand what's going on under the hood");
+    Console.WriteLine("Notice that the (background) thread IDs that execute the two are the same, since the first action quickly finishes, and thus is available "
     + "in the thread pool as the next worker. Thus, the TPL picks that thread to execute AldoTask.ContinueWith. BUT, this is not always necessarily gonna be the case!");
-    AldoTask sometask = AldoTask.Run(() => {
+    AldoTask sometask = AldoTask.Run(() =>
+    {
         Console.WriteLine($"Third AldoTask Id: {Thread.CurrentThread.ManagedThreadId}");
     });
     sometask.ContinueWith(() => Console.WriteLine($"Fourth AldoTask Id: {Thread.CurrentThread.ManagedThreadId}"));
-    Console.WriteLine("Study also the docstring of ContinueWith to truly understand what's going on under the hood");
     Console.ReadLine();
 
+    Console.WriteLine("For this third one, we are implementing Wait. Also study the docstring of Wait in AldoTask to truly understand what's going on under the hood");
+    Console.WriteLine($"Task ID before waiting with With(): {Thread.CurrentThread.ManagedThreadId}");
+    var run_task = AldoTask.Run(() =>
+    {
+        Console.WriteLine($"Fifth AldoTask Id: {Thread.CurrentThread.ManagedThreadId}");
+        Console.WriteLine("Fifth AldoTask: Will sleep for 2 seconds");
+        Thread.Sleep(2000);
+        Console.WriteLine("Fifth AldoTask: done sleeping");
+    });
+    run_task.Wait();
+    Console.WriteLine($"Task ID after waiting using With(): {Thread.CurrentThread.ManagedThreadId}");
+    Console.WriteLine("I will be consistently printed after the Task is done sleeping, because of Wait()");
+    Console.WriteLine("The issue with using Wait() above is, the main thread is locked waiting (although not visible)! See await keyword later for a better way");
+    Console.ReadLine();
 
+    Console.WriteLine("For this fourth one, we are investigating Delay method.");
+    Console.WriteLine("Why not just use Thread.Sleep?");
+    Console.WriteLine("The difference between Delay and Thread.Sleep is that Delay does not block the thread, but rather schedules the continuation of the task after the delay");
+    Console.WriteLine("BUT, we need to wait it.")
+    Console.WriteLine("If you Wait() after the Delay task, like in example below, then it will block the thread, just like Thread.Sleep! So, the benefit is lost! Know what you are doing! Don't just use methods around!");
+    Console.WriteLine("Waiting for 3 seconds, uselessly blocking the main thread with Wait() although we wanted to use Delay to purposely not block the thread");
+    var delay_task = AldoTask.Delay(TimeSpan.FromSeconds(3));
+    delay_task.Wait();
+    Console.WriteLine("Done waiting 3 seconds, while uselessly blocking the main thread and thus achieving the same bad performance with Thread.Sleep");
+    Console.WriteLine("To properly use Delay, you should use it with await keyword, which will not block the main thread. See last section though; there is a caveat!");
 
+    Console.ReadLine();
 
+    Console.WriteLine("For this fifth one, we are investigating await keyword. Note, there is a caveat so pay attention.");
+    Console.WriteLine($"Main Thread Id: {Thread.CurrentThread.ManagedThreadId}");
+    await AldoTask.Run(() => Console.WriteLine($"Sixth AldoTask Id: {Thread.CurrentThread.ManagedThreadId}"));
+    Console.WriteLine($"Seventh AldoTask Id: {Thread.CurrentThread.ManagedThreadId}");
+    Console.WriteLine("Although not visible, with await, the calling thread is actually free, unlike with using Wait()");
+
+    Console.WriteLine("Notice in the above, the thread ID of the seventh is not the same as Main Thread Id, but rather the background thread's ID, while we would've expected the await to return and let the main thread do it. Why?");
+    Console.WriteLine("This is the behavior of await keyword without a SynchonizationContext.");
+    Console.WriteLine("In a Console App, there is no SynchronizationContext, so the await keyword will not return to the main thread, but rather continue on a background thread.");
+    Console.WriteLine("But on a UI or ASP.NET Core, there is a SynchronizationContext, so the await keyword will return to the main thread, and the console writeline will be done by the main thread.");
+
+    Console.WriteLine("Unrelatedly/interestingly, this is not the case when we use Wait() above! See the TaskID console writelines there to confirm.");
+
+    Console.WriteLine("In Conclusion, the benefit of using await over Wait() is that the main thread is not blocked, and can be free to do other stuff. Always use it.");
+    Console.WriteLine("The big caveat is that unlike Wait(), the await keyword will not always return to the main thread, but rather continue on the background thread, if there is no SynchronizationContext, like the case with Console UI here.");
+
+    Console.WriteLine("This is the end of the simulation of our own implementation of Task class");
+    Console.ReadLine();
 }
-else {
+else
+{
     // This is the code from the teacher, use the other else block for experimentation
     Console.WriteLine($"Starting Thread Id: {Environment.CurrentManagedThreadId}");
 
