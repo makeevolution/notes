9ff18fb3dbab6a31e0dacb9e63b0463d8a945512
Aldo <aldo_hasibuan@yahoo.com>
2025-04-13 18:35:22 +0200

    add notes on testing legacy code

---
+++ csharp/testing_tricks/testing_legacy_code/Converter.Tests/CsvConverterTests.cs
@@ -0,0 +1,30 @@
+﻿namespace Converter.Tests;
+using VerifyTests;
+using VerifyXunit;
+using Xunit;
+[UsesVerify]
+public class ConverterApprovalTests
+{
+    [Fact]
+    public Task Should_return_csv()
+    {
+        var employees = new List<Employee>
+        {
+            new Employee { Id = 1, Name = "John Doe", BirthDate = new DateOnly(1990, 1, 1) },
+            new Employee { Id = 2, Name = "Jane Smith", BirthDate = new DateOnly(1985, 5, 15) },
+        };
+        var csvConverter = new CsvConverter<Employee>();
+        // act
+        var stream = csvConverter.ConvertToCsv(employees);
+
+        // assert/verify
+        return Verify(stream);
+    }
+
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public DateOnly BirthDate { get; set; }
+    }
+}
